ae1c0bde066ad033c2d3472620db7bfd93190ff1
mi4u <michal.lipinski@gmail.com>
2025-01-05 05:44:59 +0100

    ,

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-bun build --entrypoints ./post.ts --outfile ./dist/post/index --compile
-bun build --entrypoints ./index.ts --outfile ./dist/index --compile
-bun build --entrypoints ./index.ts --outfile ./dist/index.js --target bun
+bun build --entrypoints ./src/post.ts --outfile ./dist/post.js --target bun --minify
+bun build --entrypoints ./src/index.ts --outfile ./dist/index.js  --target bun --minify
+
+
+bun build --entrypoints ./src/run-post.ts --outfile ./dist/run-post.js --target node --minify
+bun build --entrypoints ./src/run-index.ts --outfile ./dist/run-index.js  --target node --minify
+
+
