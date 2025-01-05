37db14c97a675bc4471cb11f37e98d7fc3eedc2b
mi4u <michal.lipinski@gmail.com>
2025-01-05 04:52:22 +0100

    x

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 bun build --entrypoints ./post.ts --outfile ./dist/post/index --compile
 bun build --entrypoints ./index.ts --outfile ./dist/index --compile
+bun build --entrypoints ./index.ts --outfile ./dist/index.js --target bun
